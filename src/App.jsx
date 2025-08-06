0d3a4e98df283829322ef42836275141ebbd03eb
sparsh-15 <sparshsahu8435@gmail.com>
2025-08-06 22:54:00 +0530

    scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import PackageDetail from './pages/PackageDetail';
 import Home from './pages/Home/Home';
+import ScrollToTop from './hooks/ScrollToTop';
 
 function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/packages/:id" element={<PackageDetail />} />
